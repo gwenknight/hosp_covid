4638ce68a0983d87b41b8f1e7b4266e4a57d5383
GwenKn <g.knight@imperial.ac.uk>
2020-03-26 06:37:10 +0000

    skeleton for fitting to region data

--- region_fit.R
+++ region_fit.R
@@ -7,7 +7,14 @@ region_cases$variable <- gsub("X", "", region_cases$variable)
 region_cases$date <- as.Date(region_cases$variable, tryFormats = c("%d.%m.%Y"))
 
 ggplot(region_cases, aes(x=date, y = value)) + 
-  geom_line(aes(group = region, col=region)) + 
+  geom_point(aes(group = region, col=region)) + 
   scale_x_date("Date") + 
   scale_y_continuous("Number of cases") + 
   theme_bw()
+
+region_names <- data_region[,1]
+for(i in 1:length(region_names)){
+    
+  
+  
+}
